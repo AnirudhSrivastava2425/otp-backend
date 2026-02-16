8c8d86c53eb03ad341f5a0da7ef439dbfcd8eb6e
AnirudhSrivastava2425 <anirudhsrivastava113114@gmail.com>
2026-02-16 16:48:19 +0530

    Git ignore added

--- config/db.js
+++ config/db.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://anirudh2506:123mongoConnect#@temporary.bgt7du5.mongodb.net/?appName=temporary');
+    let pass = encodeURIComponent("123mongoConnect#")
+    await mongoose.connect(`mongodb+srv://anirudh2506:${pass}@temporary.bgt7du5.mongodb.net/?appName=temporary`);
     console.log("MongoDB Connected");
   } catch (error) {
     console.error(error.message);
